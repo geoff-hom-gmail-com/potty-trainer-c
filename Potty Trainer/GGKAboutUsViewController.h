72c1f962ec4e309a507842caf3f4d06e0f412d46
Geoffrey Hom <geoffhom@gmail.com>
2013-03-05 03:01:47 -0800

    Working on "About Us" section.

---
+++ Potty Trainer/GGKAboutUsViewController.h
@@ -0,0 +1,16 @@
+//
+//  GGKAboutUsViewController.h
+//  Perfect Potty Pal
+//
+//  Created by Geoff Hom on 3/5/13.
+//  Copyright (c) 2013 Geoff Hom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGKAboutUsViewController : UIViewController
+
+// User scrolls to read the rest of this section.
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+
+@end
---
+++ Potty Trainer/GGKAboutUsViewController.m
@@ -0,0 +1,41 @@
+//
+//  GGKAboutUsViewController.m
+//  Perfect Potty Pal
+//
+//  Created by Geoff Hom on 3/5/13.
+//  Copyright (c) 2013 Geoff Hom. All rights reserved.
+//
+
+#import "GGKAboutUsViewController.h"
+
+@interface GGKAboutUsViewController ()
+
+@end
+
+@implementation GGKAboutUsViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    // Make sure height matches what's in the storyboard.
+    self.scrollView.contentSize = CGSizeMake(320, 700);
+}
+
+@end
